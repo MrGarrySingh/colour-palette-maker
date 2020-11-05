493c52b29993d198c347f9c5ccc755dc067bc1c2
Garry Singh <mrgarrysingh92@gmail.com>
2020-11-05 20:43:17 +1100

    Copy Colour to Clipboard

--- src/components/ColourBox/ColourBox.jsx
+++ src/components/ColourBox/ColourBox.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 import "./ColourBox.css";
 
 function ColourBox({ bgColor, name }) {
   return (
-    <div style={{ background: bgColor }} className="colourBox">
-      <div className="colourBox__copyContainer">
-        <div className="colourBox__boxContent">
-          <span>{name}</span>
+    <CopyToClipboard text={bgColor}>
+      <div style={{ background: bgColor }} className="colourBox">
+        <div className="colourBox__copyContainer">
+          <div className="colourBox__boxContent">
+            <span>{name}</span>
+          </div>
+          <button className="colourBox__button">COPY</button>
+          <span className="colourBox__more">MORE</span>
         </div>
-        <button className="colourBox__button">COPY</button>
-        <span className="colourBox__more">MORE</span>
       </div>
-    </div>
+    </CopyToClipboard>
   );
 }
 
