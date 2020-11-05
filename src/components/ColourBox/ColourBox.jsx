aa264d5afc5fee33c54a0e2816f912efcb2c9e31
Garry Singh <mrgarrysingh92@gmail.com>
2020-11-05 20:36:43 +1100

    ColourBox Styles

--- src/components/ColourBox/ColourBox.jsx
+++ src/components/ColourBox/ColourBox.jsx
@@ -5,8 +5,13 @@ import "./ColourBox.css";
 function ColourBox({ bgColor, name }) {
   return (
     <div style={{ background: bgColor }} className="colourBox">
-      <span>MORE</span>
-      <span>{name}</span>
+      <div className="colourBox__copyContainer">
+        <div className="colourBox__boxContent">
+          <span>{name}</span>
+        </div>
+        <button className="colourBox__button">COPY</button>
+        <span className="colourBox__more">MORE</span>
+      </div>
     </div>
   );
 }
