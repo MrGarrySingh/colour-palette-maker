3d753a9c921215e40de6c817083487285db1b1cf
Garry Singh <mrgarrysingh92@gmail.com>
2020-12-03 22:23:10 +1100

    Added Snackbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,16 +2,26 @@ import React, { useState } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
+import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
 
 import "rc-slider/assets/index.css";
 import "./Navbar.css";
 
 const Navbar = ({ level, changeLevel, handleChange }) => {
   const [format, setFormat] = useState("hex");
+  const [open, setOpen] = useState(false);
 
   const changeFormat = (e) => {
+    /* Snackbar doesn't work onClick but works with keyboard input? */
+    setOpen(true);
     setFormat(e.target.value);
     handleChange(e.target.value);
+  };
+
+  const closeSnackbar = () => {
+    setOpen(false);
   };
 
   return (
@@ -38,6 +48,28 @@ const Navbar = ({ level, changeLevel, handleChange }) => {
           <MenuItem value="rgba">RGBA - RGB(255, 255, 255, 1.0)</MenuItem>
         </Select>
       </div>
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={open}
+        autoHideDuration={2000}
+        message={
+          <span id="message-id">Format Changed To {format.toUpperCase()}!</span>
+        }
+        ContentProps={{
+          "aria-describedby": "message-id",
+        }}
+        onClose={closeSnackbar}
+        action={[
+          <IconButton
+            onClick={closeSnackbar}
+            color="inherit"
+            key="close"
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>,
+        ]}
+      />
     </header>
   );
 };
