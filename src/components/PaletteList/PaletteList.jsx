9ada5d8976356f2248824dc11504b6b5e90a6e39
Garry Singh <mrgarrysingh92@gmail.com>
2020-12-13 11:31:06 +1100

    Material UI Styles & MiniPalette Component

--- src/components/PaletteList/PaletteList.jsx
+++ src/components/PaletteList/PaletteList.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import MiniPalette from "../MiniPalette/MiniPalette.jsx";
 import { Link } from "react-router-dom";
 
 function PaletteList({ palettes }) {
   return (
     <div>
+      <MiniPalette />
       <h1>Color Palettes</h1>
       {palettes.map((palette) => (
-        <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
+        <MiniPalette {...palette} />
       ))}
     </div>
   );
