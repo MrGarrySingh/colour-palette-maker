cbefb8980faa27920c79b19de3b5b8a086899e8b
Garry Singh <mrgarrysingh92@gmail.com>
2020-12-19 21:35:57 +1100

    Create Dynamic Text Colour using Luminance

--- src/components/ColourBox/ColourBox.jsx
+++ src/components/ColourBox/ColourBox.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 
 import "./ColourBox.css";
 
@@ -14,6 +15,9 @@ function ColourBox({ bgColor, name, paletteId, id, showLink }) {
     );
   };
 
+  const isDarkColour = chroma(bgColor).luminance() <= 0.15;
+  const isLightColour = chroma(bgColor).luminance() >= 0.65;
+
   return (
     <CopyToClipboard text={bgColor} onCopy={toggleCopy}>
       <div style={{ background: bgColor }} className="colourBox">
@@ -23,20 +27,34 @@ function ColourBox({ bgColor, name, paletteId, id, showLink }) {
         />
         <div className={`colourBox__copyMsg ${copied && "show"}`}>
           <h1>COPIED!</h1>
-          <p>{bgColor}</p>
+          <p className={isLightColour && "colourBox__darkText"}>{bgColor}</p>
         </div>
         <div className="colourBox__copyContainer">
           <div className="colourBox__boxContent">
-            <span>{name}</span>
+            <span className={isDarkColour && "colourBox__lightText"}>
+              {name}
+            </span>
           </div>
-          <button className="colourBox__button">COPY</button>
+          <button
+            className={`colourBox__button ${
+              isLightColour && "colourBox__darkText"
+            }`}
+          >
+            COPY
+          </button>
         </div>
         {showLink && (
           <Link
             to={`/palette/${paletteId}/${id}`}
             onClick={(e) => e.stopPropagation()}
           >
-            <span className="colourBox__more">MORE</span>
+            <span
+              className={`colourBox__more ${
+                isLightColour && "colourBox__darkText"
+              }`}
+            >
+              MORE
+            </span>
           </Link>
         )}
       </div>
