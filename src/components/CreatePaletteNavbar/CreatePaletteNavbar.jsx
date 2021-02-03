60309b75807537499606052daa58db2c12614b2c
Garry Singh <mrgarrysingh92@gmail.com>
2021-02-03 20:43:17 +1100

    Tweak Form Styles

--- src/components/CreatePaletteNavbar/CreatePaletteNavbar.jsx
+++ src/components/CreatePaletteNavbar/CreatePaletteNavbar.jsx
@@ -7,7 +7,7 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+import PaletteIcon from "@material-ui/icons/Palette";
 import Button from "@material-ui/core/Button";
 import useStyles from "../../styles/CreatePaletteNavbarStyles.js";
 
@@ -44,9 +44,9 @@ function CreatePaletteNavbar({
             aria-label="open drawer"
             onClick={() => handleDrawerOpen(open)}
             edge="start"
-            className={clsx(classes.menuButton, open && classes.hide)}
+            className={clsx(classes.menuButton, { [classes.hide]: open })}
           >
-            <MenuIcon />
+            <PaletteIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
             Create Your Palette
